476cc523d3ebe00d668df916a80cf3b6bfd18f78
Marliece <180341631+Marliece@users.noreply.github.com>
2024-09-14 11:01:34 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,33 @@
+import random
+print("One-Million-To-One")
+print()
+print("""Guess a number between 1 and 1,000,000 and I will tell you if you are too low, too high, or get it correct.""")
+print()
+print("Let's play!")
+print()
+correct_number = random.randint(1,1000000)
+print()
+count = 1
+while True:
+  guess = int(input("What is your guess? "))
+  if guess == correct_number:
+    print("Correct!")
+    print("You are a winner! 🎉")
+    print()
+    if count == 1:
+      print("It took you", count, "guess to get it correct")
+      break
+    else:
+      print("It took you", count, "guesses to get it correct")
+      break
+  elif guess >= correct_number:
+    print("Too high")
+    print()
+    count += 1
+  elif guess <= 0:
+    print("Now we'll never know what the answer is …")
+    exit()
+  else:
+    print("Too low")
+    print()
+    count += 1
